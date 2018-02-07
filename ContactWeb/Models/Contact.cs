9d2e5635951091bf0bbe18950ab7c95eb6094ff8
Nand <nandkv.online@gmail.com>
2018-02-06 20:13:01 -0600

    Additional changes

---
+++ ContactWeb/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactWeb.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Phone { get; set; }
+    }
+}
