6dc77b942e0b43ccaff8288cde695af8bf7aa024
Nand <nandkv.online@gmail.com>
2018-02-06 21:10:59 -0600

    Added Nunit Tests

---
+++ ContactWeb.Tests/Models/ContactTest.cs
@@ -0,0 +1,23 @@
+﻿using ContactWeb.Models;
+using NUnit.Framework;
+
+namespace ContactWeb.Tests.Models
+{
+    [TestFixture]
+    public class ContactTest
+    {
+        [Test]
+        public void VerifyContactInit()
+        {
+            Contact c = new Contact();
+            Assert.That(c, !Is.Null);
+        }
+
+        [Test]
+        public void VerifyContactNotInited()
+        {
+            Contact c = null;
+            Assert.That(c, Is.Null);
+        }
+    }
+}
